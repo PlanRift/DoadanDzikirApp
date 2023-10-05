e588c11fba7511669a76bf66753325a6f4c910e2
Rajiidham <rajididham@gmail.com>
2023-10-05 08:59:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/doadandzikirapp/ui/DzikirPagidanPetangActivity.kt
@@ -0,0 +1,12 @@
+package com.example.doadandzikirapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.doadandzikirapp.R
+
+class DzikirPagidanPetangActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dzikir_pagidan_petang)
+    }
+}
